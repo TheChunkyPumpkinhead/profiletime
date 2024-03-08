3f79021ba996e85038d6b3c16d17bdb1512da8f2
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-07 16:12:04 -0800

    created profile

--- src/App.js
+++ src/App.js
@@ -25,11 +25,7 @@ function Intro() {
   return (
     <div>
       <h1>Chris Campos</h1>
-      <p>
-        Full-stack web developer and teacher at Udemy. When not coding or
-        preparing a course, I like to play board games, to cook (and eat), or to
-        just enjoy the Portuguese sun at the beach.
-      </p>
+      <p>asljaljfl ajfldksjalj. aljfsljflajsldds</p>
     </div>
   );
 }
